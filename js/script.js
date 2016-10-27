560a7a0c0b9b1bc1becbb13314e6c6c58153115b
Waleed Shabbir <waleedshabbir1@gmail.com>
2016-10-27 13:58:22 +0500

    Adding Google Street View to background

--- js/script.js
+++ js/script.js
@@ -11,6 +11,21 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr+ ', '+cityStr;
+
+
+
+    $greeting.text('So, you wanna live at '+address+'?');
+
+    var streetViewUrl ='https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' +address+'';
+
+
+    $body.append('<img class="bgimg" src="'+streetViewUrl+'">');
+
+
     // load streetview
 
     // YOUR CODE GOES HERE!
